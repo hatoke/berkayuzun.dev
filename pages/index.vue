6836d1132e923f92f6134bfc847a566a15b75e5a
Berkay UZUN <oluyum@gmail.com>
2023-01-06 04:27:59 +0300

    chore: add title tag

--- pages/index.vue
+++ pages/index.vue
@@ -8,6 +8,9 @@ import ScreenFrame from '~/components/ScreenFrame.vue'
 
 export default Vue.extend({
   components: { ScreenFrame },
+  head: {
+    title: "Berkay UZUN - Frontend Developer"
+  },
   name: 'IndexPage',
 })
 </script>
