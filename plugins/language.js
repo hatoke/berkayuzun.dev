e91e272acd2adf1598a3a7002335c9959ef16509
Berkay UZUN <oluyum@gmail.com>
2023-01-12 19:35:21 +0300

    fix: cv language selector

--- plugins/language.js
+++ plugins/language.js
@@ -1,3 +1,9 @@
 export function clientLanguage() {
-  return navigator.language.split('-')[0].toUpperCase()
+  const lang = navigator.language.split('-')[0].toUpperCase()
+  // eslint-disable-next-line no-constant-condition
+  if (lang !== 'TR' || lang !== 'EN') {
+    return 'EN'
+  } else {
+    return lang
+  }
 }
